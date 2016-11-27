1e5ce1b745a28ad8a6dcb394a9024e49c055ecfb
itoneer <skamin26@gmail.com>
2016-11-27 20:46:26 +0100

    [IMPLEMENT] Wymaganie 2
    Zmodyfiowano metodę add():
    - zmieniono typ z ""void" na "int"
    - dodano return - na razie funkcja zwraca tylko 0

--- test_proj2_example/src/calculator/StringCalculator.java
+++ test_proj2_example/src/calculator/StringCalculator.java
@@ -6,14 +6,17 @@
 package calculator;
 
 public class StringCalculator {
-    public static final void add(final String numbers) {
+    public static final int add(final String numbers) { // Changed void to int
         String[] numbersArray = numbers.split(",");
         if (numbersArray.length > 2) {
             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
         } else {
             for (String number : numbersArray) {
-                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
+                if (!number.isEmpty()) {
+                Integer.parseInt(number);
+                }
             }
         }
+        return 0; // Added return
     }
 }
