f1c81596e4d3d8e827c2ba42083b3edb0d96ed46
itoneer <skamin26@gmail.com>
2016-11-29 16:00:27 +0100

    [TEST] Wymaganie 5
    Zezwól funkcji add() na przetwarzanie znaku nowej linii pomiędzy
    liczbami (poza przecinkami)

--- test_proj2_example/test/calculator/StringCalculatorTest.java
+++ test_proj2_example/test/calculator/StringCalculatorTest.java
@@ -38,7 +38,13 @@ public class StringCalculatorTest {
         assertEquals(3+6, StringCalculator.add("3,6"));
     }
     
+    @Test
     public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
         assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+    }
+    
+    @Test
+    public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
+        assertEquals(3+6+15, StringCalculator.add("3,6\n15"));
     }
 }
