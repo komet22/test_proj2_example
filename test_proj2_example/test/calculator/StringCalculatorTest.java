6bfd991833c1037570e8c82c97fba148131f2975
itoneer <skamin26@gmail.com>
2016-11-29 16:56:13 +0100

    [TEST] Wymaganie 7
    Liczby ujemne będą zwracać wyjątek

--- test_proj2_example/test/calculator/StringCalculatorTest.java
+++ test_proj2_example/test/calculator/StringCalculatorTest.java
@@ -52,4 +52,21 @@ public class StringCalculatorTest {
     public final void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
         assertEquals(3+6+15, StringCalculator.add("//;\n3;6;15"));
     }
-}
+    
+    @Test(expected = RuntimeException.class)
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
+        StringCalculator.add("3,-6,15,18,46,33");
+    }
+    
+    @Test
+    public final void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown() {
+        RuntimeException exception = null;
+        try {
+            StringCalculator.add("3,-6,15,-18,46,33");
+        } catch (RuntimeException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals("Negatives not allowed: [-6, -18]", exception.getMessage());
+        }
+    }
